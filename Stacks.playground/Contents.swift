4df8fe6d24332706e10bdf0e91944092ef4ca9de
Kenneth Scanlon <scanlon771@gmail.com>
2021-12-27 13:42:46 -0800

    Stacks implementation

---
+++ Stacks.playground/Contents.swift
@@ -0,0 +1,54 @@
+//Using Swift with Stacks
+import Foundation
+
+public struct Stack<T> {
+    private var items: [T] = []
+    //Push method
+    mutating func push(element: T) {
+        items.append(element)
+    }
+    //Pop method
+    mutating func pop() -> T? {
+        return items.popLast()
+    }
+    //Peek method
+    func peek() -> T? {
+        return items.last
+    }
+}
+
+//Stack Structures
+var customStack = Stack<Int>()
+
+//Using push method
+customStack.push(element: 4)
+print(customStack)
+customStack.push(element: 8)
+print(customStack)
+customStack.push(element: 12)
+print(customStack)
+
+//Using peek method
+print(customStack.peek()!)
+
+//Using pop method
+var x = customStack.pop()
+print(x!)
+x = customStack.pop()
+print(x!)
+
+//Output
+"Stack<Int>(items: [4])"
+"Stack<Int>(items: [4, 8])"
+"Stack<Int>(items: [4, 8, 12])"
+12
+12
+8
+
+//Stack Extensions
+extension Stack: CustomStringConvertible {
+    public var description: String {
+        return items.description
+    }
+}
+
